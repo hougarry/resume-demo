04f84eb182a382e978eff0015ee6bf4cf525ad07
Helena Hall <hougarry@gmail.com>
2023-10-13 13:03:30 +0800

    update

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'ckeditor',
-    
+    'ckeditor'  
 ]
 
 MIDDLEWARE = [
@@ -137,3 +136,4 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/" # step 5 : add this line
--- resume_demo/urls.py
+++ resume_demo/urls.py
@@ -15,21 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 # step 1 : add django.urls.include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main')), # add main urls
-    # step 2 : add ckeditor urls
-    url(r'^media/(?P<path>.*)$', serve,
+    path('', include('main.urls', namespace='main')), 
+    re_path(r'^media/(?P<path>.*)$', serve,  # Modified line here
         {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
+    re_path(r'^static/(?P<path>.*)$', serve,  # Modified line here
         {'document_root': settings.STATIC_ROOT}),
 ]
 
