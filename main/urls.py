3450140bae9b9f99a7b157e1647f98521e01e4be
Helena Hall <hougarry@gmail.com>
2023-10-13 13:02:48 +0800

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,9 @@ app_name = 'main'
 
 #create models one by one
 urlpatterns = [
-    #path('', views.IndexView.as_view(), name='home'),
-    #path('contact/', views.ContactView.as_view(), name='contact'),
-    #path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
-    #path('portfolio/<slug:slug>/', views.PortfolioDetailView.as_view(), name='portfolio'),
-    #path('blog/', views.BlogDetailView.as_view(), name='blog'),
+    path('', views.IndexView.as_view(), name='home'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('portfolio/', views.PortfolioView.as_view(), name='portfolios'),
+    path('portfolio/<slug:slug>/', views.PortfolioDetailView.as_view(), name='portfolio'),
+    path('blog/', views.BlogDetailView.as_view(), name='blog'),
 ]
