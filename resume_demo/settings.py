2a1372502c290f3fd4a70980a713d03629bceaad
Helena Hall <hougarry@gmail.com>
2023-10-21 20:47:57 +0800

    Update settings.py

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -123,13 +123,13 @@ USE_TZ = True
 # step 4 : add static and media url
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'mediafiles')
 ]
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
